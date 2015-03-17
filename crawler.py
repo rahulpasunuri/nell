ce6b8c88d03520fd0947e365855fc75bc5225da6
rahulpasunuri <rahupasu@indiana.edu>
2015-03-16 21:42:35 -0400

    code with exception handling

--- crawler.py
+++ crawler.py
@@ -1,14 +1,18 @@
+import re
 import urllib2
 import urlparse
 import time
 import pprint
 import json
-import BeautifulSoup # get html links
 
 dbFileName = "extractionPatterns.csv"
 outFileName = "data.txt"
 rels=["ceoof"]
 cat=[]
+
+
+endMarkPattern = "[\.\<\>!;\"=]"
+endMarkRegex = re.compile(endMarkPattern)
 
 f = open(dbFileName, "r")
 lines = f.readlines()
@@ -16,6 +20,8 @@ lines = f.readlines()
 relPatterns=[]
 catPatterns=[]
 
+data=[]
+totalMiss=0
 #skip the first line..
 lines=lines[1:]
 
@@ -38,20 +44,63 @@ hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML,
 
 #search google
 for p in relPatterns:
-    words=p.split()
-    s="%20".join(words)
-    s = "%22"+s+"%22"
-    
-    #create the search url
-    #url="http://www.google.com/search?as_q=&as_epq="+s
-    
-    #can get more results with continuous calling as a client..
-    url="http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=large&start=0&q="+s
-    req = urllib2.Request(url, headers=hdr)   
-   
-    #get the search page//
-    googlePage = urllib2.urlopen(req)
-    data = json.loads(googlePage.read())
-    for r in data["responseData"]["results"]:
-        print r["url"]
-    
+	words=p.split()
+	s="%20".join(words)
+	s = "%22"+s+"%22"
+
+	#create the search url
+	#url="http://www.google.com/search?as_q=&as_epq="+s
+
+	#can get more results with continuous calling as a client..
+	url="http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=large&start=0&q="+s
+	req = urllib2.Request(url, headers=hdr)   
+
+	#get the search page//
+	googlePage = urllib2.urlopen(req)
+	googleJson = json.loads(googlePage.read())
+
+	#fetch only the top 3 results of each query.
+	count=0
+	
+	try:
+		if googleJson is None or googleJson["responseData"] is None and googleJson["responseData"]["results"] is None:#error checking..
+			print "some thing wrong with the pattern/google: "+p
+			continue
+	except:
+		#wait for a minute for google to allow us to search again.
+		print "some thing wrong with the pattern/google: "+p
+		
+		print "Waiting for 5 minutes."
+		time.sleep(300)
+		totalMiss=totalMiss+1
+		
+		continue
+			
+	for r in googleJson["responseData"]["results"]:
+		if count>3:
+			break
+		count = count+1
+		#print r["url"]
+		print "Checking URL: "+r["url"]
+
+		try:
+			page=urllib2.urlopen(r["url"])
+			content = page.read()    	
+			sentences = endMarkRegex.split(content)
+			for s in sentences:				
+				if p in s:
+					data.append(s.strip())					
+					isPresent=True
+			if isPresent == False:
+				print "link didnot match with pattern- "+p+" ", r["url"]
+		except:
+			print "Error reading the link: ",r["url"]			
+		
+	#break #TODO
+	
+outFile = open(outFileName, "w")
+for s in data:
+	outFile.write(s+"\n")	
+outFile.close()	
+print "Total Miss Count is ", totalMiss
+#break#TODO
